3744b3635c5b8105e6b4bbe2b4928580e4c318d7
Phil Denhoff <phil@denhoff.ca>
2022-08-23 23:40:42 -0700

    Upgrade to astro v1.0.8 (from 0.23)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: "./dist",
+  publicDir: "./public",
+  site: "https://denhoff.ca",
+  server: {
+    host: "::1"
   },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    hostname: "::1",
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: [
-    '@astrojs/renderer-react',
-  ],
-};
+  integrations: [react(), mdx()]
+});
