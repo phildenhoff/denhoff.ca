bcbcf1ac6d533fbf0ae11fb82bf6aa55a8be61e5
Phil Denhoff <phil@denhoff.ca>
2022-08-24 23:29:22 -0700

    add `prefetch`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   outDir: "./dist",
@@ -10,5 +12,5 @@ export default defineConfig({
   server: {
     host: "::1"
   },
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx(), prefetch()]
 });
