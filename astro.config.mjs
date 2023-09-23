27bdb4c5af9ba08efb23dbc971c58a6cee676198
Phil Denhoff <phil@denhoff.ca>
2023-09-22 17:19:16 -0700

    feat: use Rehype Pretty Code for code styling, catppuccin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import solidJs from "@astrojs/solid-js";
+import rehypePrettyCode from "rehype-pretty-code";
+
+import latte from './themes/latte.json' assert {type: 'json'};
+import mocha from './themes/mocha.json' assert {type: 'json'};
+ 
+const prettyCodeOptions = {
+  theme: {
+    light: latte,
+    dark: mocha 
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +24,9 @@ export default defineConfig({
     host: "::1",
   },
   integrations: [mdx(), prefetch(), solidJs()],
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
 });
